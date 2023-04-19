dddcd0194553fe6850065af4331cfffd47c96600
brendanx12 <brendanosaka@gmail.com>
2023-04-19 13:36:29 +1000

    Assignment 2 Database was Created
    Assignment 2 Database was Created

---
+++ IoTbay Assignment 2/src/main/java/IoTbay/Customer.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package IoTbay;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author brend
+ */
+public class Customer implements Serializable{
+    private int id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private int mobileNumber;
+    private String address;
+    private String state;
+    private String city;
+    private int postcode;
+    private String password;
+    private boolean active;
+
+    public Customer(int id, String firstName, String middleName, String lastName, String email, int mobileNumber,
+            String address, String state, String city, int postcode, String password, boolean active) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.email = email;
+        this.mobileNumber = mobileNumber;
+        this.address = address;
+        this.state = state;
+        this.city = city;
+        this.postcode = postcode;
+        this.password = password;
+    }
+
+
+    public String getId(){
+        return Integer.toString(id);
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(int mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+
+}
